d15e86547298d7e40f497c7af660441a087a4416
yr12dong <289663639@qq.com>
2021-05-06 00:10:24 +0800

    2021/05/05

--- src/prom1027/Solution.java
+++ src/prom1027/Solution.java
@@ -7,32 +7,49 @@ public class Solution {
         int length = A.length;
         // 最后一个 arr[j] 倒数第二个 arr[i] 的最长等差数列长度
         int[][] dp = new int[length][length];
-        HashMap<Integer, Integer> index = new HashMap<>();
+        HashMap<Integer, List> index = new HashMap<>();
         dp[0][0] = 1;
         dp[0][1] = 2;
         int result = 2;
         for (int i = 0; i < length; i++) {
             dp[i][i] = 1;
+            if(index.containsKey(A[i])){
+                List list = index.get(A[i]);
+                list.add(i);
+                index.put(A[i], list);
+            } else {
+                List list = new LinkedList();
+                list.add(i);
+                index.put(A[i], list);
+            }
             for (int j = i - 1; j >= 0; j--) {
                 int dif = A[i] - A[j];
+
                 int tar = A[j] - dif;
+
 //                if(tar > A[j]) {
 //                    dp[j][i] = 2;
 //                }
-                Integer location;
+                List locations;
                 if (index.containsKey(tar)) {
-                    location = index.get(tar);
+                    locations = index.get(tar);
                 } else {
                     dp[j][i] = 2;
                     result = Math.max(result, dp[j][i]);
                     continue;
                 }
-                if((int)location > j) dp[j][i] = Math.max(dp[j][i], 2);
-                dp[j][i] = Math.max(dp[j][i], dp[location][j] + 1);
+                if(dif != 0) {
+                    for(Object location : locations){
+                        if((int)location > j) dp[j][i] = Math.max(dp[j][i], 2);
 
+                        dp[j][i] = Math.max(dp[j][i], dp[(int)location][j] + 1);
+                    }
+                } else {
+//                    System.out.println("i : " + i + " j : " + j + " " +  locations.size());
+                    dp[j][i] = Math.max(dp[j][i], locations.size());
+                }
                 result = Math.max(result, dp[j][i]);
             }
-            index.put(A[i], i);
         }
         return result;
 
@@ -45,7 +62,7 @@ public class Solution {
 //        System.out.println(new HashSet<>(list).size());
     }
 
-//    static int[] A = new int[]{3,6,9,12};
+    static int[] A = new int[]{3,6,9,12};
 //    static int[] A = new int[]{9,4,7,2,10};
 //    static int[] A = new int[]{20,1,15,3,10,5,8};
 //    static int[] A = new int[]{83,20,17,43,52,78,68,45};
