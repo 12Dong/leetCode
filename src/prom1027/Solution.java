7cdc0a7b2360d03f33ca4b5932c0ed897c00f4bc
yr12dong <289663639@qq.com>
2021-05-07 00:39:35 +0800

    2021/05/06

--- src/prom1027/Solution.java
+++ src/prom1027/Solution.java
@@ -62,10 +62,10 @@ public class Solution {
 //        System.out.println(new HashSet<>(list).size());
     }
 
-    static int[] A = new int[]{3,6,9,12};
+//    static int[] A = new int[]{3,6,9,12};
 //    static int[] A = new int[]{9,4,7,2,10};
 //    static int[] A = new int[]{20,1,15,3,10,5,8};
 //    static int[] A = new int[]{83,20,17,43,52,78,68,45};
 //    static int[] A = new int[]{44,46,22,68,45,66,43,9,37,30,50,67,32,47,44,11,15,4,11,6,20,64,54,54,61,63,23,43,3,12,51,61,16,57,14,12,55,17,18,25,19,28,45,56,29,39,52,8,1,21,17,21,23,70,51,61,21,52,25,28};
-//    static int[] A = new int[]{1,1,1,1,1};
+    static int[] A = new int[]{1,1,1,1,1};
 }
