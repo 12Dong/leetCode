31f01565bcc7febca74cbc7186d623b3703d8d62
yr12dong <289663639@qq.com>
2022-07-08 00:10:32 +0800

    2022/07 / 07

---
+++ src/pro189/Solution.java
@@ -0,0 +1,25 @@
+package pro189;
+
+public class Solution {
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = gcd(nums.length, k);
+        for(int index = 0; index < count; index ++) {
+            int next = (index + k) % nums.length;
+            int lastVal = nums[index];
+            while(next != index) {
+                int tmp = lastVal;
+                lastVal = nums[next];
+                nums[next] = tmp;
+                next = (next + k) % nums.length;
+            }
+            nums[index] = lastVal;
+        }
+
+    }
+
+    public int gcd(int n, int m) {
+       return m > 0 ?  gcd(m, n % m) : n;
+    }
+}
