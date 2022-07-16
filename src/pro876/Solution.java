0e9296aa441080a72df956ae779ab835037af100
yr12dong <289663639@qq.com>
2022-07-16 12:14:12 +0800

    2022/07 / 16

---
+++ src/pro876/Solution.java
@@ -0,0 +1,27 @@
+package pro876;
+
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null) {
+            return head;
+        }
+        if(head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
