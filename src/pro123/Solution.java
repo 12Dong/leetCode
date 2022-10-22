dd3d3dd73f10efbe648f1c349e8b4a2fd57959b1
yr12dong <289663639@qq.com>
2022-10-22 22:05:46 +0800

    2022/10/22

---
+++ src/pro123/Solution.java
@@ -0,0 +1,41 @@
+package pro123;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        long ans = Integer.MIN_VALUE;
+        // 第 0 次交易 没股票
+        long dp0 = 0;
+        // 第 0 次交易 有股票
+        long dp1 = -prices[0];
+        // 第 1 次交易 没有股票
+        long dp2 = Integer.MIN_VALUE;
+        // 第 1 次交易 有股票
+        long dp3 = Integer.MIN_VALUE;
+        // 第 2 次交易 没有股票
+        long dp4 = Integer.MIN_VALUE;
+        for(int i = 1 ; i < n; i++) {
+            long tdp0 = dp0;
+            long tdp1 = Math.max(dp1, dp0 - prices[i]);
+            long tdp2 = Math.max(dp2, dp1 + prices[i]);
+            long tdp3 = Math.max(dp3, dp2 - prices[i]);
+            long tdp4 = Math.max(dp4, dp3 + prices[i]);
+            dp0 = tdp0;
+            dp1 = tdp1;
+            dp2 = tdp2;
+            dp3 = tdp3;
+            dp4 = tdp4;
+            ans = Math.max(ans, dp4);
+            ans = Math.max(ans, dp2);
+        }
+        return (int)Math.max(ans, 0);
+    }
+
+    public static void main(String[] argv){
+        new Solution().maxProfit(prices);
+    }
+
+    public static int[] prices = new int[]{7,6,4,3,1};
+//        public static int[] prices = new int[]{1,2,3,4,5};
+
+}
---
+++ src/pro76/Solution2.java
@@ -0,0 +1,54 @@
+package pro76;
+
+import java.util.HashMap;
+
+public class Solution2 {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> cntMap = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            cntMap.put(t.charAt(i), cntMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        Integer minLength = Integer.MAX_VALUE;
+        int l = 0;
+        int r = 0;
+        String ans = "";
+        int currentCnt = t.length();
+        // currentCnt 到 0 说明 当前l、r满足 覆盖子串
+        for(int i = 0; i < s.length(); i++) {
+            if(cntMap.containsKey(s.charAt(i))) {
+                int cnt = cntMap.get(s.charAt(i));
+                if(cnt > 0) {
+                    currentCnt--;
+                }
+                cntMap.put(s.charAt(i), cntMap.get(s.charAt(i)) - 1);
+                r = i;
+                if(currentCnt == 0) {
+                    while(l <= r) {
+                        if(minLength > (r-l+1)) {
+                            ans = s.substring(l, r + 1);
+                            minLength = r - l + 1;
+                        }
+                        if(cntMap.containsKey(s.charAt(l))) {
+                            int lCnt = cntMap.get(s.charAt(l));
+                            lCnt++;
+                            cntMap.put(s.charAt(l), lCnt);
+                            if(lCnt > 0) {
+                                currentCnt++;
+                            }
+                        }
+
+                        l++;
+                        if(currentCnt > 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] argv) {
+        new Solution2().minWindow("a", "a");
+    }
+}
