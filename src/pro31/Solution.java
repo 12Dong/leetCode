0850f4b50ac8650377729826892a29e2c1ae52f5
yr12dong <289663639@qq.com>
2022-11-14 23:38:42 +0800

    2022/11/14

--- src/pro31/Solution.java
+++ src/pro31/Solution.java
@@ -6,20 +6,25 @@ public class Solution {
     public void nextPermutation(int[] nums) {
         boolean change = false;
         for(int i = nums.length - 1;i>=0;i--) {
-            for(int j = i - 1; j>=0;j--) {
-                if(nums[i] > nums[j]) {
-                    int tmp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = tmp;
-                    Arrays.sort(nums, j+1, nums.length);
-                    change = true;
-                    break;
+            if(i < 1) continue;;
+            if(nums[i] > nums[i-1]) {
+                int index = i;
+                int val = nums[index];
+                for(int j = i; j < nums.length; j++) {
+                    if(nums[j] > nums[i-1] && nums[j] < val) {
+                        index = j;
+                        val = nums[j];
+                    }
                 }
+                int tmp = nums[i-1];
+                nums[i-1] = nums[index];
+                nums[index] = tmp;
+                Arrays.sort(nums, i, nums.length);
+                change = true;
             }
             if(change) break;
         }
         if(!change) {
-            int[] reverse = new int[nums.length];
             for(int i = nums.length - 1; i >=nums.length/2 ; i--) {
                 int tmp = nums[i];
                 nums[i] = nums[nums.length - i - 1];
