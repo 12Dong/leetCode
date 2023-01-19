bf0e4a5b3018cd4934d3f0794a31a100cbefb6af
12dong <289663639@qq.com>
2023-01-19 19:31:49 +0800

    2022 / 01/ 19

---
+++ src/pro221/Solution.java
@@ -0,0 +1,35 @@
+package pro221;
+
+public class Solution {
+
+    public int maximalSquare(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] dp = new int[n][m];
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == '1') {
+                    if(i == 0) {
+                        dp[i][j] = 1;
+                        ans = Math.max(ans, dp[i][j]);
+                        continue;
+
+                    }
+
+                    if(j == 0) {
+                        dp[i][j] = 1;
+                        ans = Math.max(ans, dp[i][j]);
+                        continue;
+                    }
+
+                    int tmp = Math.min(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.min(tmp, dp[i-1][j-1]) + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans * ans;
+    }
+}
