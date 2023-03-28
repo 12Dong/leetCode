4f277b04a27e8e9b388426656625aa69414ad11e
12dong <289663639@qq.com>
2023-03-28 15:34:25 +0800

    2023/ 03 / 28

---
+++ src/pro461/Solution.java
@@ -0,0 +1,15 @@
+package pro461;
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int ans = 0;
+        while(x != 0 || y != 0) {
+            if( (x % 2) != (y % 2)) {
+                ans++;
+            }
+            x = x/ 2;
+            y = y/ 2;
+        }
+        return ans;
+    }
+}
