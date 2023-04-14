429f36504378a2bfbacb494a4364f79c67108e54
12dong <289663639@qq.com>
2023-04-14 19:13:26 +0800

    2023/ 04 / 14

---
+++ src/pro24/Solution.java
@@ -0,0 +1,35 @@
+package pro24;
+
+public class Solution {
+    
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    
+    public ListNode swapPairs(ListNode head) {
+        if(head == null) return head;
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode pre = start;
+        ListNode node1 = head;
+        ListNode node2 = node1.next;
+        while(node2 != null) {
+            ListNode next = node2.next;
+            pre.next = node2;
+            node2.next = node1;
+            node1.next = next;
+            pre = node2;
+            node1 = next;
+            if(next != null) {
+                node2 = next.next;
+            } else {
+                break;
+            }
+        }
+        return start.next;
+    }
+}
--- src/pro440/Solution.java
+++ src/pro440/Solution.java
@@ -3,20 +3,64 @@ package pro440;
 import java.util.PriorityQueue;
 
 public class Solution {
-    public static int findKthNumber(int n, int k) {
-        PriorityQueue<String> pq = new PriorityQueue<>();
-        for(int i = 1; i <= n; i++) {
-            String s = String.valueOf(i);
-            if(pq.size() < k) {
-                pq.add(s);
+
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int steps = findPrefix(curr, n);
+            if (steps <= k) {
+                k -= steps;
+                curr++;
             } else {
-                pq.remove(k);
-                pq.add(s);
+                curr = curr * 10;
+                k--;
             }
         }
-        for(int i = 0; i < k - 1; i++) {
-            pq.poll();
+        return curr;
+    }
+
+    //    public int findKthNumber(int n, int k) {
+//        int prefix = 1;
+//        k--;
+//        while(k > 0) {
+//            int steps = getSteps(prefix, k);
+//            if(steps <= k) {
+//                k -= steps;
+//                prefix++;
+//            } else {
+//                prefix *= 10;
+//                k--;
+//            }
+//        }
+//        return prefix;
+//    }
+
+
+    public int findPrefix(int prefix, int n) {
+        int count = 0;
+        int next = prefix + 1;
+        while(prefix <= n) {
+            count += (Math.min(n + 1, next) - prefix);
+            prefix *= 10;
+            next *= 10;
         }
-        return Integer.valueOf(pq.poll());
+        return count;
+    }
+
+    public int getSteps(int curr, long n) {
+        int steps = 0;
+        long first = curr;
+        long last = curr;
+        while (first <= n) {
+            steps += Math.min(last, n) - first + 1;
+            first = first * 10;
+            last = last * 10 + 9;
+        }
+        return steps;
+    }
+
+    public static void main(String[] argv) {
+        System.out.println(new Solution().findKthNumber(25, 11));
     }
 }
