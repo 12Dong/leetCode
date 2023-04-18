843e3a54527abbc517f44ec7e568a331eafd8069
12dong <289663639@qq.com>
2023-04-18 17:52:44 +0800

    2023/ 04 / 18

---
+++ src/pro1143/Solution2.java
@@ -0,0 +1,28 @@
+package pro1143;
+
+public class Solution2 {
+
+//    输入：text1 = "abcde", text2 = "ace"
+//    输出：3
+//      0 1 2 3 4 5
+//    0 0 0 0 0 0 0
+//    1 0 1 1 1 1 1
+//    2 0 1 1 2 2 2
+//    3 0 1 1 2 2 3
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for(int i = 1; i <= len1; i++) {
+            for(int j = 1; j <= len2; j++) {
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i][j]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
---
+++ src/pro135/Solution.java
@@ -0,0 +1,35 @@
+package pro135;
+
+import java.util.Arrays;
+
+public class Solution {
+
+//    [1,3,2,2,1]
+//    1 2 1 2 1
+//    [ 1, 0, 2]
+//      1  1  2
+    public int candy(int[] ratings) {
+        int[] count = new int[ratings.length];
+        Arrays.fill(count, 1);
+        for(int i = 1; i < ratings.length; i++) {
+            if(ratings[i] > ratings[i - 1]) {
+                count[i] = count[i - 1] + 1;
+            }
+        }
+        for(int i = ratings.length - 2; i >=0; i--) {
+            if(ratings[i] > ratings[i + 1] && count[i] <= count[i + 1]) {
+                count[i] = count[i + 1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i < count.length; i++) {
+            ans += count[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] argv) {
+        new Solution().candy(new int[]{1, 0, 2});
+    }
+}
