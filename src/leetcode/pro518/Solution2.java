9bf77c7c90745d81169584a20b2a61a55d31a041
12Dong <289663639@qq.com>
2023-10-09 23:14:28 +0800

    2023/ 10 / 09

---
+++ src/leetcode/pro518/Solution2.java
@@ -0,0 +1,16 @@
+package leetcode.pro518;
+
+public class Solution2 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for(int i = 0; i < coins.length; i++) {
+            for(int j = 0; j <= amount; j++) {
+                if(j - coins[i] >= 0) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
