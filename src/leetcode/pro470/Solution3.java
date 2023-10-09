d7ead7e8b346314170f22f58bdee1bf3d98140c3
12Dong <289663639@qq.com>
2023-10-09 23:41:31 +0800

    2023/ 10 / 09

---
+++ src/leetcode/pro470/Solution3.java
@@ -0,0 +1,19 @@
+package leetcode.pro470;
+
+public class Solution3 {
+    public int rand10() {
+        while(true) {
+            int high = 7 * (rand7() - 1);
+            int low = (rand7() - 1);
+            if(high + low + 1 > 10) {
+                continue;
+            } else {
+                return high + low + 1;
+            }
+        }
+    }
+
+    public int rand7() {
+        return 1;
+    }
+}
